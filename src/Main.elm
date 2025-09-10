7264d5d5199219aa2685c078bc65c443d40f9d7e
evan <iamevn@users.noreply.github.com>
2025-09-10 12:33:40 -0700

    add show picker todo

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@
 -- TODO: seed randomness either from url param or something else
 -- TODO: saving sorting state to url fragment
 -- TODO: pick random comparsion when there are multiple candidates
+-- TODO: pick which shows to sort
 
 
 module Main exposing (main)
